bb2da2d93b796e0de89e46cc15549693f6f721ae
Mike Mayori <mmayorivera@gmail.com>
2020-02-13 23:34:34 -0400

    — base server
    - ready to include controllers
    - security
    - db binding

---
+++ server/src/config/index.ts
@@ -0,0 +1,23 @@
+
+// Load environment variables from .env file
+dotenv.config();
+
+const env = process.env.NODE_ENV || 'development';
+const configs = {
+    base: {
+        env,
+        name: process.env.APP_NAME || 'rest-api-mongo',
+        host: process.env.APP_HOST || '0.0.0.0',
+        port: process.env.APP_PORT || 7070,
+    },
+    production: {
+        port: process.env.APP_PORT || 7071,
+    },
+    development: {},
+    test: {
+        port: 7072,
+    },
+};
+const config = Object.assign(configs.base, configs[env]);
+
+export {config};
